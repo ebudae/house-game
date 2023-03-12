fa6e889d788058c47f264ca837efbd59aafae4d3
Ebudae <aesedr1@gmail>
2023-03-12 23:25:03 +0100

    0.1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{Result, Write};
+use rand::prelude::*;
 
 fn write_obj_file(vertices: &[Vertex], indices: &[u32], filename: &str) -> Result<()> {
     let mut file = File::create(filename)?;
@@ -28,26 +29,41 @@ struct Vertex {
 fn create_mesh() -> (Vec<Vertex>, Vec<u32>) {
     let mut vertices = Vec::new();
     let mut indices = Vec::new();
+    let mut wvertices = Vec::new();
+    let mut rng = rand::thread_rng();
+
+    for _i in 0..=60 {
+        let x = rng.gen::<f32>() * 200.0 - 100.0;
+        let z = rng.gen::<f32>() * 200.0 - 100.0;
+        let y = rng.gen::<f32>() * 2.0 - 1.0;
+        wvertices.push(Vertex { x, y, z });
+    }
 
     // Create a 2D grid of vertices with a spacing of 1 unit between each vertex
-    for i in 0..200 {
-        for j in 0..200 {
+    for i in 0..=200 {
+        for j in 0..=200 {
             let x = (i as f32 - 100.0) * 1.0;
             let z = (j as f32 - 100.0) * 1.0;
             let _y = 0.0; // set the y-coordinate to 0, as the mesh is flat
-            let y = ((x/8.0).powi(2) + (z/8.0).powi(2)).sqrt().sin() / ((x/8.0).powi(2) + (z/8.0).powi(2)).sqrt()*5.0; // calculate the y-coordinate using the sinc function
+            let _y = ((x/8.0).powi(2) + (z/8.0).powi(2)).sqrt().sin() / ((x/8.0).powi(2) + (z/8.0).powi(2)).sqrt() *5.0; // calculate the y-coordinate using the sinc function
+            let y = sinc( x, z, 8.0, 8.0, 8.0 ) 
+                - sinc( x-30.0, z + 40.0, 3.0, 8.0, 6.0 )
+                + sinc( x+3.0, z + 20.0, 19.0, 38.0, 2.0 ) 
+                + sinc( x + 30.0, z, 17.68, 14.24, 7.3 )
+                + worley( &wvertices, x, z )
+                + sinc( x, z, 4.0, 4.0, 2.0 ) * sinc( 5.0 - x, 7.0 - z, 8.0, 6.0, 2.0 );
 
             vertices.push(Vertex { x, y, z });
         }
     }
 
     // Create indices that define triangles from adjacent vertices
-    for i in 0..199 {
-        for j in 0..199 {
-            let v0 = i * 200 + j;
-            let v1 = i * 200 + j + 1;
-            let v2 = (i + 1) * 200 + j;
-            let v3 = (i + 1) * 200 + j + 1;
+    for i in 0..=199 {
+        for j in 0..=199 {
+            let v0 = i * 201 + j;
+            let v1 = i * 201 + j + 1;
+            let v2 = (i + 1) * 201 + j;
+            let v3 = (i + 1) * 201 + j + 1;
 
             // First triangle
             indices.push(v0);
@@ -62,4 +78,36 @@ fn create_mesh() -> (Vec<Vertex>, Vec<u32>) {
     }
 
     (vertices, indices)
+}
+
+type T = f32;
+fn sinc( i: T, j: T,xt: T, zt: T, a: T )
+    -> T
+{
+    //if ((i/xt).powi(2) + (j/zt).powi(2)) == 0.0{
+    if i == 0.0 && j == 0.0 {
+        a
+    }else{
+        ((i/xt).powi(2) + (j/zt).powi(2)).sqrt().sin() / 
+        ((i/xt).powi(2) + (j/zt).powi(2)).sqrt() * a
+    }
+}
+
+fn distance(x1: T, y1: T, x2: T, y2: T) -> T {
+    ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)).sqrt()
+}
+
+fn worley( k: &Vec<Vertex>, i: f32, j:f32 )
+    -> f32
+{
+    let mut q = 99999.99;
+    let mut which = -1i32;
+    for m in 0..k.len() {
+        let n = distance(i, j, k[m].x, k[m].z);
+        if n < q {
+            q = n; 
+            which = m as i32;
+        }
+    }
+    k[which as usize].y
 }
